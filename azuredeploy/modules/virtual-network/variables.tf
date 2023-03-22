7515b9c0b9210d3958e9c8389186386b155e8877
shailender singh <shailender_bakshi@yahoo.com>
2023-03-22 06:25:24 +0000

    Updated variables.tf

---
+++ azuredeploy/modules/virtual-network/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+    description = "Specify region of Azure Virtual Machines."
+    default = "West US"
+}
+
+variable "resource_group" {
+    description = "Specify resource group of Azure Virtual Machines."  
+    default = "Default"
+}
