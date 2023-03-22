d7ebfa7eff3fb43e653eb788ca1615ff76ab26f9
shailender singh <shailender_bakshi@yahoo.com>
2023-03-22 06:45:22 +0000

    Updated variables.tf

--- azuredeploy/modules/virtual-network/variables.tf
+++ azuredeploy/modules/virtual-network/variables.tf
@@ -1,9 +1,31 @@
-variable "location" {
-    description = "Specify region of Azure Virtual Machines."
-    default = "West US"
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the virtual network"
+  type        = string
 }
 
-variable "resource_group" {
-    description = "Specify resource group of Azure Virtual Machines."  
-    default = "Default"
+variable "location" {
+  description = "Defines the Tier to use for this virtual network"
+  type        = string
+}
+
+variable "vnet_Count" {
+  description = "The number of instances to be deployed"
+  type        = number
+  default     = 1
+}
+
+variable "vnet_Name" {
+  type        = string
+  description = "Prefix for the vnet."
+  default = ""
+}
+
+variable "address_space" {
+  description = "The list of address spaces used by the virtual network"
+  type        = list
+}
+
+variable "dns_servers" {
+  description = "List of IP addresses of DNS servers"
+  type        = list
 }
