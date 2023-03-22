22f103710bb8ff21028cd8159cc8e0ad318877c8
shailender singh <shailender_bakshi@yahoo.com>
2023-03-22 06:47:24 +0000

    Updated main.tf

---
+++ azuredeploy/main.tf
@@ -0,0 +1,25 @@
+module "resource_group"{
+    source           = "../Modules/ResourceGroup"
+    rsg_Name         = var.rsg_Name
+    location         = var.location
+
+}
+
+module "vNet"{
+    source                 = "../Modules/vNET"
+    vnet_Name              = var.vnet_Name
+    resource_group_name    = module.resource_group.name
+    location               = var.location
+    address_space          = var.vnet_cidr
+    dns_servers            = var.dns_servers
+}
+
+
+module "Subnet"{
+    source                 = "../Modules/Subnet"
+    subnet_name            = var.subnet_name
+    rsg_Name               = module.resource_group.name
+    vnet_name              = module.vNet.name
+    subnet_prefixes        = var.subnet_prefixes
+
+}
