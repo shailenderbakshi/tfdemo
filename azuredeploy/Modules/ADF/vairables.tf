5eeb95a2311650a109584fa47c3568504e28a74d
shailender singh <shailender_bakshi@yahoo.com>
2023-03-23 07:53:49 +0000

    Updated vairables.tf

--- azuredeploy/Modules/ADF/vairables.tf
+++ azuredeploy/Modules/ADF/vairables.tf
@@ -2,3 +2,9 @@ variable "adf_name" {
     description = "Azure data factory"
     default = "gs-adf001"
 }
+
+variable "rsg_Name" {
+  type        = string
+  description = "Prefix for the Resource Groups."
+  default = "RSG"
+}
