042a0c2abf84660842f8ac9add81041bde3e564e
shailender singh <shailender_bakshi@yahoo.com>
2023-03-23 07:51:42 +0000

    Updated main.tf

--- azuredeploy/main.tf
+++ azuredeploy/main.tf
@@ -26,7 +26,7 @@ module "Subnet"{
 
 module "ADF"{
     source                 = "./Modules/ADF"
-    rsg_Name    = module.resource_group.name
+    rsg_Name               = module.resource_group.name
     location               = var.location
     adf_name               = var.adf_name
     
