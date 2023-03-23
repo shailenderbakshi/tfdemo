e903697ffd5c48e9db9821b04f287bc80d03b968
shailender singh <shailender_bakshi@yahoo.com>
2023-03-23 07:46:05 +0000

    Updated main.tf

--- azuredeploy/Modules/ADF/main.tf
+++ azuredeploy/Modules/ADF/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_factory" "adf" {
   name                = var.adf_name
   location            = var.location
-  resource_group_name = var.rg.name
+  resource_group_name = var.rsg_Name.name
 }
