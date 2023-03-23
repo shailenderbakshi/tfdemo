c1e6be8ce80a86918d9deea04b509e44d125e79a
shailender singh <shailender_bakshi@yahoo.com>
2023-03-23 08:23:44 +0000

    Updated main.tf

--- azuredeploy/Modules/AppServicePlan/main.tf
+++ azuredeploy/Modules/AppServicePlan/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_app_service_plan" "app_service_plan" {
   name                = var.app_plan_name
   location            = var.location
   resource_group_name = var.rsg_Name
