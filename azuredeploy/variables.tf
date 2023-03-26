1bb298f3c45e34afdca8e3dbbaf437d689ca6c30
shailender singh <shailender_bakshi@yahoo.com>
2023-03-26 06:40:56 +0000

    Updated variables.tf

--- azuredeploy/variables.tf
+++ azuredeploy/variables.tf
@@ -64,6 +64,45 @@ variable "subnet_prefixes" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
 }
+##########
+Databricks
+##########
+
+variable "workspace_name" {
+  type        = string
+  description = "Name of Databricks workspace"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of resource group into which Databricks will be deployed"
+}
+
+variable "location" {
+  type        = string
+  description = "Location in which Databricks will be deployed"
+}
+
+variable "vnet_id" {
+  type        = string
+  description = "ID of existing virtual network into which Databricks will be deployed"
+}
+
+variable "private_subnet_name" {
+  type        = string
+  description = "Name of the private subnet"
+}
+
+variable "public_subnet_name" {
+  type        = string
+  description = "Name of the public subnet"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Map of tags to attach to Databricks workspace"
+  default     = {}
+}
 
 ########################################################################
 # APP GATEWAY
