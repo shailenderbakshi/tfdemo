bdc80164266900e1045199e97fda8716acf0752b
shailender singh <shailender_bakshi@yahoo.com>
2023-03-28 07:09:50 +0000

    Updated main.tf

--- azuredeploy/main.tf
+++ azuredeploy/main.tf
@@ -45,8 +45,8 @@ module "databricks-workspace" {
     workspace_name      = var.workspace_name
     resource_group_name = module.resource_group.name
     location            = var.location
-    // vnet_id             = var.vnet_id
-    // private_subnet_name = var.private_subnet_name
-    // public_subnet_name  = var.public_subnet_name
+    vnet_id             = var.vnet_id
+    private_subnet_name = var.private_subnet_name
+    public_subnet_name  = var.public_subnet_name
     // tags                = var.tags
 }
