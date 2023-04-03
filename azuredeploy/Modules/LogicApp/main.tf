b6c91f6c28a773c397255f5f83116be62bec4aa5
shailender singh <shailender_bakshi@yahoo.com>
2023-04-03 17:46:38 +0000

    Updated main.tf

--- azuredeploy/Modules/LogicApp/main.tf
+++ azuredeploy/Modules/LogicApp/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_logic_app_workflow" "Logic-app-module" {
   name                = var.Logicapp_name
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
 }
