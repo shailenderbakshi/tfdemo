378d47874ed1c4e3db2856a5fbfcdf19c15a66b0
shailender singh <shailender_bakshi@yahoo.com>
2023-04-03 16:51:13 +0000

    Updated variables.tf

--- azuredeploy/Modules/azure-databricks-workspace/variables.tf
+++ azuredeploy/Modules/azure-databricks-workspace/variables.tf
@@ -13,20 +13,20 @@ variable "location" {
   description = "Location in which Databricks will be deployed"
 }
 
-variable "vnet_id" {
-  type        = string
-  description = "ID of existing virtual network into which Databricks will be deployed"
-}
+// variable "vnet_id" {
+//   type        = string
+//   description = "ID of existing virtual network into which Databricks will be deployed"
+// }
 
-variable "private_subnet_name" {
-  type        = string
-  description = "Name of the private subnet"
-}
+// variable "private_subnet_name" {
+//   type        = string
+//   description = "Name of the private subnet"
+// }
 
-variable "public_subnet_name" {
-  type        = string
-  description = "Name of the public subnet"
-}
+// variable "public_subnet_name" {
+//   type        = string
+//   description = "Name of the public subnet"
+// }
 
 variable "tags" {
   type        = map(string)
